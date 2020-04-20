6c787e92de88f6c424d47f52b9eea0c8f4246480
savvyguru <pungtuckwei@gmail.com>
2020-04-20 23:22:18 +0800

    add audio and video call

--- public/index.js
+++ public/index.js
@@ -1,8 +1,51 @@
 //global variables
 var myAvatar;
 var chatAvatar;
+var microphoneDevice;
+var speaker;
+var camera;
+var contact;
+
 
 function chatroom(username, password) {
+    navigator.mediaDevices.getUserMedia({audio: true, video: true}).then(function(stream) {
+
+        /* Stream received which means that the user has authorized the application to access to the audio and video devices. Local stream can be stopped at this time */
+        stream.getTracks().forEach(function(track) {
+            track.stop();
+        });
+    
+        /*  Get the list of available devices */
+        navigator.mediaDevices.enumerateDevices().then(function(devices){
+    
+            /* Do something for each device (e.g. add it to a selector list) */
+            devices.forEach(function(device) {
+                switch (device.kind) {
+                    case "audioinput":
+                        // This is a device of type 'microphone'
+                        microphoneDevice = device;
+                        console.log("SEEME",microphoneDevice);
+                    case "audiooutput":
+                        // This is a device of type 'speaker'
+                        speaker = device;
+                        console.log("SEEME1",speaker);
+                    case "videoinput":
+                        // This is a device of type 'camera'
+                        camera = device;
+                        console.log("SEEME2",camera);
+                    default:                 
+                        break;
+                }
+            });
+    
+        }).catch(function(error) {
+            /* In case of error when enumerating the devices */
+            break;
+        });
+    }).catch(function(error) {
+        /* In case of error when authorizing the application to access the media devices */
+        break;
+    });
     var associatedConversation = null;
 
     /* Wait for the page to load */
@@ -65,6 +108,8 @@ function chatroom(username, password) {
 
         rainbowSDK.contacts.searchByJid(selectedContactId)
             .then(function (result) {
+                contact = result;
+                console.log("lucy",contact);
                 return result
             })
             .then(result => rainbowSDK.conversations.openConversationForContact(result))
@@ -79,12 +124,12 @@ function chatroom(username, password) {
                     var messages = result.messages;
                     for (var message in messages){
                         if (messages[message].from.loginEmail === username){
-                            $('.card-body').append(getSendMessageHTML(messages[message].data,messages[message].from.avatar.src));
+                            $('.card-body').append(getSendMessageHTML(messages[message].data,messages[message].from.avatar.src,messages[message].date));
                             myAvatar = messages[message].from.avatar.src;
                         }
                         else{
-                            $('.card-body').append(getReceivedMessageHTML(messages[message].data,messages[message].from.avatar.src));
-                            chatAvatar = messages[message].from.avatar.src;c
+                            $('.card-body').append(getReceivedMessageHTML(messages[message].data,messages[message].from.avatar.src,messages[message].date));
+                            chatAvatar = messages[message].from.avatar.src;
                         }
 
                     }
@@ -125,22 +170,121 @@ function chatroom(username, password) {
     document.addEventListener(rainbowSDK.im.RAINBOW_ONNEWIMMESSAGERECEIVED, onNewMessageReceived)
 
 
-    function getReceivedMessageHTML(message, avatar) {
-        return '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="' + avatar + '" class="rounded-circle user_img_msg"></div><div class="msg_container">' + message + '<span class="msg_time">' + '9:00 AM, Today' + '</span></div></div>'
+    function getReceivedMessageHTML(message, avatar,timestamp) {
+        return '<div class="d-flex justify-content-start mb-4"><div class="img_cont_msg"><img src="' + avatar + '" class="rounded-circle user_img_msg"></div><div class="msg_container">' + message + '<span class="msg_time">' + timestamp + '</span></div></div>'
     }
-    function getSendMessageHTML(message, avatar) {
-        return '<div class="d-flex justify-content-end mb-4"><div class="msg_container_send">' + message + '<span class="msg_time_send">' + '9:10 AM, Today' + '</span></div><div class="img_cont_msg"><img src="' + avatar + '" class="rounded-circle user_img_msg"></div></div>';
+    function getSendMessageHTML(message, avatar,timestamp) {
+        return '<div class="d-flex justify-content-end mb-4"><div class="msg_container_send">' + message + '<span class="msg_time_send">' + timestamp + '</span></div><div class="img_cont_msg"><img src="' + avatar + '" class="rounded-circle user_img_msg"></div></div>';
     }
 
     $('.send_btn').on('click', function () {
         // Send message
-        var message = $('#typeBar').val();
+        if ($('#typeBar').val()===""){
+            console.log("empty typebar")
+        }   
+        else{
+            var message = $('#typeBar').val();
+            $('.card-body').append(getSendMessageHTML(message, myAvatar));
+            $('#typeBar').val("");
+            rainbowSDK.im.sendMessageToConversation(associatedConversation, message);
+        }
+    });
+        
+    // do audio call
+    $('.fa-phone').on('click', function () {
+        
+        //check browser compatability
+        if(rainbowSDK.webRTC.canMakeAudioVideoCall()) {
+            if(rainbowSDK.webRTC.hasAMicrophone()){
+                console.log("can call and have microphone");
+                /* Select the microphone to use */
+                rainbowSDK.webRTC.useMicrophone(microphoneDevice.deviceId);
+                /* Select the speaker to use */
+                rainbowSDK.webRTC.useSpeaker(speaker.deviceId);
+                console.log("speaker success");
+                var callInAudio = function callInAudio() {
+                    /* Call this API to call a contact using only audio stream*/
+                    console.log("Contact ist",contact);
+                    var res = rainbowSDK.webRTC.callInAudio(contact);
+                    if(res.label === "OK") {
+                        /* Your call has been correctly initiated. Waiting for the other peer to answer */
+                        console.log("Calling initiated, waiting for peer to answer");
+                    }
+                };
+                callInAudio();
+            }
+            else{
+                console.log("has no microphone");
+                }
+        }   
+        else{
+            console.log("cannot make audiovideo call");
+        }
+    });
+    // do video call
+    $('.fa-video').on('click', function () {
+        var Call = angular.element(document.querySelector('body')).injector().get('Call');
+        var onWebRTCTrackChanged = function onWebRTCTrackChanged(event) {
 
-        $('.card-body').append(getSendMessageHTML(message, myAvatar));
-        $('#typeBar').val("");
-        rainbowSDK.im.sendMessageToConversation(associatedConversation, message);
+            let call = event.detail;
+        
+            // Manage remote video
+            if (call.remoteMedia & call.Media.VIDEO) {
+                rainbowSDK.webRTC.showRemoteVideo(call);
+            } else {
+                rainbowSDK.webRTC.hideRemoteVideo(call);
+            }
+            // Manage local video
+            if (call.localMedia & call.Media.VIDEO) {
+                rainbowSDK.webRTC.showLocalVideo(call);
+            } else {
+                rainbowSDK.webRTC.hideLocalVideo(call);
+            }
+        };
+        
+        document.addEventListener(rainbowSDK.webRTC.RAINBOW_ONWEBRTCTRACKCHANGED, onWebRTCTrackChanged)
+        //check browser compatability
+        if(rainbowSDK.webRTC.canMakeAudioVideoCall()) {
+            if(rainbowSDK.webRTC.hasAMicrophone()){
+                console.log("can call and have microphone");
+                /* Select the microphone to use */
+                rainbowSDK.webRTC.useMicrophone(microphoneDevice.deviceId);
+                /* Select the speaker to use */
+                rainbowSDK.webRTC.useSpeaker(speaker.deviceId);
+                rainbowSDK.webRTC.useCamera(camera.deviceId);
+                console.log("speaker success");
+                var callInVideo = function callInVideo(contact) {
+                    console.log("video calling");
+                    /* Call this API to call a contact using both audio and video streams*/
+                    var res = rainbowSDK.webRTC.callInVideo(contact);
+                    if(res.label === "OK") {
+                        /* Your call has been correctly initiated. Waiting for the other peer to answer */
+                        //display video
+
+                        var addVideoToCall = function addVideoToCall(call) {
+                            /* Call this API to try do add video to the call */
+                            var res = rainbowSDK.webRTC.addVideoToCall(call);
+                            rainbowSDK.webRTC.showLocalVideo();
+                            rainbowSDK.webRTC.showRemoteVideo(call)
+                        };
+                        addVideoToCall(call);
+
+                    }
+                };
+                callInVideo(contact);
+            }
+            else{
+                console.log("has no microphone");
+                }
+        }   
+        else{
+            console.log("cannot make audiovideo call");
+        }
     });
 }
+    
+
+
 /*
     UI Interactions
 */
