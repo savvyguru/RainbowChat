244445db178c3e009e3c2b1ffcb30d16079a0927
wangtian12345 <48759398+wangtian12345@users.noreply.github.com>
2020-04-21 07:49:48 +0800

    Add files via upload

---
+++ public/AudioCallTest.java
@@ -0,0 +1,71 @@
+package com.example.seleniumtest;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.StaleElementReferenceException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class AudioCallTest {
+
+    static String myUserName = "wt@gmail.com";
+    static String myPassword = "Sutd12345!";
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //System.setProperty("webdriver.gecko.driver","/Users/apple/Desktop/chromedriver");
+        //WebDriver driver = new FirefoxDriver();
+        System.setProperty("webdriver.chrome.driver","/Users/apple/Desktop/chromedriver");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://localhost:8887/");
+
+        // get the user name field of the account page
+        WebElement username = driver.findElement(By.id("username"));
+
+        Thread.sleep(1000);
+
+        // send my user name to fill up the box
+        username.sendKeys(myUserName);
+
+        Thread.sleep(1000);
+
+        // locate the "Next" button in the account page
+        WebElement password = driver.findElement(By.id("password"));
+
+        //write password
+        password.sendKeys(myPassword);
+
+        Thread.sleep(1000);
+
+        // login and :)
+        WebElement nextButton = driver.findElement(By.id("loginbutton"));
+        nextButton.click();
+
+        Thread.sleep(1000);
+
+        try {
+            WebDriverWait wait = new WebDriverWait(driver, 10);
+            // wait only until the project front page loads
+            wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("i[class='fas fa-phone']")));
+            WebElement phone = driver.findElement(By.cssSelector("i[class='fas fa-phone']"));
+            phone.click();
+            ////add another action for phone call(agent selection?)
+            System.out.println("success");
+
+        } catch (Exception NoSuchElementException) {
+            System.out.println("login/password name invalid");
+        }
+    }
+}
